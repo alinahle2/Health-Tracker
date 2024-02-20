9624b496c0f8a746f849458793afed956469e006
alinahle <nahleali290@gmail.com>
2024-02-20 21:00:45 +0200

    add readme file

---
+++ readme.txt
@@ -0,0 +1,17 @@
+Setting Up the Project:
+
+Clone Repository: 
+git clone https://github.com/alinahle2/Health-Tracker.git
+Install Dependencies: npm install
+
+MySQL Database Setup:
+Create a new MySQL database named simpledb.
+
+Import SQL Schema:
+Import simpledb.sql located in the server folder into your MySQL database using phpMyAdmin.
+
+Running the Application:
+Running the Client (Angular):
+Start the Angular development server: ng serve -o
+Running the Server (Node.js):
+Start the Node.js server using nodemon to automatically restart on changes: nodemon index.js
---
+++ server/simpledb.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 20, 2024 at 07:48 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `simpledb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `water` int(11) NOT NULL,
+  `step` int(11) NOT NULL,
+  `sleep` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `name`, `water`, `step`, `sleep`) VALUES
+(12, 'ali455', 575, 7, 7),
+(16, 'w2w', 2, 2, 2),
+(23, 'aliw2', 5, 10, 20),
+(24, 'haya', 455, 2, 204);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
